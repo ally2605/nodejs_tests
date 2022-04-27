a55bb6ff8b0673c5d6d58f7101e2750e4e8240cb
Alessandra Germano Turnes <Alessandra@Alessandras-MacBook-Pro-2.local>
2022-04-27 16:21:43 -0300

    Creation of the files and first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+async function userAction () {
+    try {
+       console.log ("entrei na funcao");
+
+       const response = await fetch ('http://api.open-notify.org/astros.json');
+       const apiReturn = await response.json();
+
+       console.log("passei pela api");
+
+       document.getElementById("result").innerHTML =  "<br><br> There are " + apiReturn.number + "  astronauts on board the ISS now. They are:<br>";
+        
+       for (i=0; i < apiReturn["people"].length; i++) {
+          document.getElementById("result").innerHTML += "<br>" + apiReturn["people"][i].name;
+       }
+       console.log (apiReturn.number);
+    }
+    catch (error) {
+        console.log(error);
+        document.getElementById("result").innerHTML = "<br><br>It wasn't possible to fetch the results. Try again later."
+
+    }
+}
